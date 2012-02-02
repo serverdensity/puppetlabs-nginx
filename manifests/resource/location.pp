3c49988aa97bd8254097fec3c03e17aa4bbc49a2
Christian G. Warden <cwarden@xerus.org>
2012-02-02 10:11:26 -0800

    Add support for aliases in locations
    An alias can be specified for a location using alias_root instead of
    www_root.  Aliases in nginx are similar to root directives except that
    the part of the request that matches the location is stripped when
    creating the path to file.  See
    http://wiki.nginx.org/HttpCoreModule#alias for an example.

--- manifests/resource/location.pp
+++ manifests/resource/location.pp
@@ -6,7 +6,9 @@
 #   [*ensure*]             - Enables or disables the specified location (present|absent)
 #   [*vhost*]              - Defines the default vHost for this location entry to include with
 #   [*location*]           - Specifies the URI associated with this location entry
-#   [*www_root*]           - Specifies the location on disk for files to be read from. Cannot be set in conjunction with $proxy
+#   [*www_root*]           - Specifies the location on disk for files to be read from. Cannot be set in conjunction with $proxy or $alias_root.
+#   [*alias_root*]         - Specifies a path on disk to the files. Like $www_root, except the location is stripped from the
+#                            request.  Cannot be set in conjunction with either $www_root or $proxy.
 #   [*index_files*]        - Default index files for NGINX to read when traversing a directory
 #   [*proxy*]              - Proxy server(s) for a location to connect to. Accepts a single value, can be used in conjunction
 #                            with nginx::resource::upstream
@@ -29,6 +31,7 @@ define nginx::resource::location(
   $ensure             = present,
   $vhost              = undef,
   $www_root           = undef,
+  $alias_root         = undef,
   $index_files        = ['index.html', 'index.htm', 'index.php'],
   $proxy              = undef,
   $proxy_read_timeout = $nginx::params::nx_proxy_read_timeout,
@@ -52,6 +55,8 @@ define nginx::resource::location(
   # Use proxy template if $proxy is defined, otherwise use directory template.
   if ($proxy != undef) {
     $content_real = template('nginx/vhost/vhost_location_proxy.erb')
+  } elsif ($alias_root != undef) {
+    $content_real = template('nginx/vhost/vhost_location_alias.erb')
   } else {
     $content_real = template('nginx/vhost/vhost_location_directory.erb')
   }
@@ -60,11 +65,17 @@ define nginx::resource::location(
   if ($vhost == undef) {
     fail('Cannot create a location reference without attaching to a virtual host')
   }
-  if (($www_root == undef) and ($proxy == undef)) {
-    fail('Cannot create a location reference without a www_root or proxy defined')
+
+  $location_config = [
+    $alias_root ? { undef => 0, default => 1},
+    $www_root   ? { undef => 0, default => 1},
+    $proxy      ? { undef => 0, default => 1}
+  ]
+  if $location_config[0] + $location_config[1] + $location_config[2] > 1 {
+    fail('Cannot define both directory (www_root or alias_root) and proxy in a virtual host')
   }
-  if (($www_root != undef) and ($proxy != undef)) {
-    fail('Cannot define both directory and proxy in a virtual host')
+  if $location_config[0] + $location_config[1] + $location_config[2] == 0 {
+    fail('Cannot create a location reference without a www_root, alias_root, or proxy defined')
   }
 
   ## Create stubs for vHost File Fragment Pattern
